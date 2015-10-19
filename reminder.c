30b9ad58cd272ca7eb56726ea97061b59e172899
nero <aUtomata2.71828@openmailbox.org>
2015-10-19 18:20:47 -0500

    .

--- reminder.c
+++ reminder.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define MAX 1000
@@ -15,34 +16,38 @@ struct node *last = NULL;
 void createReminder(){
 
   char ch;
-  
+ 
   struct node *new_node = malloc(sizeof(struct node));
-
   new_node->next = NULL;
-
+  
+  if(new_node == NULL){
+    printf("No messages for this day.");
+    return;
+  }
+ 
   printf("Which month would you like to set a reminder for?\n");
   scanf("%d", &(new_node)->month);
   printf("For which day?\n");
   scanf("%d",&(new_node)->day);
-
+  //repeat:
   printf("Enter your message : ");
   scanf("%s", &(new_node)->remind);
 
+  /*  if(strlen(new_node->remind) > 200)
+    printf("A reminder must be less than 200 characters. Please try again.");
+  goto repeat;
+  */
   first = last = new_node;
-  //...
 
 }
 
 int getMonth(){
-  //return month
+  
   time_t mytime;
   struct tm * timeInfo;
   time(&mytime);
-  
   timeInfo = localtime(&mytime);
-  // printf("month: %d\n", timeInfo->tm_mon + 1);
-  //test
-  // printf(ctime(&mytime));
+
   return (timeInfo->tm_mon + 1);
 
 }
@@ -64,17 +69,10 @@ int main(void){
 
   char *reminder[MAX];
   int dd, mm, i, j;
-
-  //testing
   
   printf("%d-",getMonth());
   printf("%d\n", getDay());
   deleteNode();
-
-  //new_node->reminder = "test";
-  //new_node->next = first;
-  //first = new_node;
-
   createReminder();
 
   return 0;
