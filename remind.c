06e16bf74baf3aea7b956e5ae0b4f03250d6506a
nero <aUtomata2.71828@openmailbox.org>
2015-10-20 18:24:43 -0500

    what have I done

--- remind.c
+++ remind.c
@@ -71,50 +71,25 @@ char *setReminder(){
   printf("\nEnter your message for %d-%d\n", list1.month, list1.day);
   scanf("%s", &(list1).remind[0]);
 
-  // printf("%s", list1.remind);
-
-
   return (list1.remind[0]);
 }
 
 void updateRemindArray(char *rArray[], int size){
-  //set variable i to equal the date and assign it
-  //to index of RemindArray to increment days in real time
 
-  //get day, store in variable, get day again, if day is not
-  //the same then increment array
-
-  //check new day, if messageFound is true display message, if not
-  //ask them to set reminder
 
 }
-
-
 
 int main(int argc, char *argv[]){
 
   char *r[R+1];
   int month, day, currentDay;
 
-  /* FILE *file;
-  char *buffer;
-  size_t reminderinFile;
-  long fSize;
-
-  file = fopen("reminder.bin", "rb+");
-
-  if(file == NULL){
-    file = fopen("reminder.bin", "wb+");
-  }
-  */
   getCurrentTime();
 
   setMonth();
   setDay();
   setReminder();
-  printf("%s", list1.remind[0]);
 
-  printf("%s", r[0]);
 
   
   return 0;
