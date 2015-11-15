f9f7056b9aa612db7b0e185d9f33f9af79c88af4
nero <aUtomata2.71828@openmailbox.org>
2015-11-14 20:21:35 -0600

    working on getRem func

--- remind.c
+++ remind.c
@@ -2,9 +2,9 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <errno.h>
 #include <ctype.h>
+#include <assert.h>
 #define MAX 365
 #define BUFF 150
 size_t data;
@@ -14,6 +14,7 @@ struct list{
   int day, month;
   char remind[MAX];
 }list1;
+
 
 int getMonth(){
   
@@ -42,6 +43,16 @@ char *getCurrentTime(){
   char *string = (ctime(&mytime));
   return string;
   
+}
+
+char *getCurAbbMonth(){
+  char *st;
+  time_t mytime;
+  struct tm * timeInfo;
+  time(&mytime);
+  timeInfo = localtime(&mytime);
+  strftime(st, 20, "Month : %h\n",timeInfo);
+  return st;
 }
 
 int setDay(){
@@ -73,29 +84,47 @@ int setMonth(){
 
 void getReminder(int m, int d){
   
-  char *st;
+  char *st, *st_copy;
   FILE *file = fopen("reminder.bin", "rb+");
   int line = 2;
 
+  
   if(file == NULL){
     perror("file not found");
     exit(1);
   }
   
-  for(int i=0; i < line;i++) {
-      st = fgets(buffer, BUFF, file);
-      printf("%s", st);
+ check:
+  for(int i = 0; i < 2; i++){
+    st[i] = getc(file);
+    if(st[i] == ':') break;
+  }
+
+  int dd = atoi(st);
+
+  if(dd == d){
+    for(int i=0; i < line;i++) {
+      st_copy = fgets(buffer, BUFF, file);
+      printf("%s", st_copy);
     }
-  
-  //puts(&list1.remind[d]);
- }
+  }else {
+    fseek(file, BUFF, SEEK_SET);
+    if(ferror(file)){
+      printf("End of file, no reminder found.");
+      exit(1);
+    }else
+	goto check;
+  }
+    
+}
+char *abbMonth(int m);
 
 int main(int argc, char *argv[]){
 
-  int day, month;
-  char *str, ans = 'n', *time, *timeStamp;
+  int day, month, stamp;
+  char *str, ans = 'n', *time;
   long fileSize;
-  char *input;
+  char *input, *ab, *new_str;
 
   FILE *file = fopen("reminder.bin", "rb+");
 
@@ -104,15 +133,17 @@ int main(int argc, char *argv[]){
   printf("\n");
   day = getDay();
   month = getMonth();
+  ab = abbMonth(month);
   struct list list1 = *(struct list*)malloc(sizeof(struct list));
-
-
+  
+  
   if(file == NULL){
     
     file = fopen("reminder.bin","wb+");
 
     printf("Would you like to set a reminder (y/n)?\n");
     scanf("%c", &ans);
+    
 
     if(ans == 'y'){
       
@@ -124,18 +155,18 @@ int main(int argc, char *argv[]){
      
       getline(&str,&data,stdin);
       fgets(str,150,stdin);
-      strcat(str, time);
       strcpy(&list1.remind[day], str);
+      
 
-      snprintf(buffer, BUFF, "%s\n", &list1.remind[day]);
+      snprintf(buffer, BUFF, "%s %d: %s\n", ab,day, &list1.remind[day]);
       fwrite(&list1.remind[day], sizeof(char), sizeof(buffer), file);
       //fwrite(buffer, sizeof(char), sizeof(buffer),file);
-
       printf("\nReminder set: \n");
       puts(buffer);
+      
       fseek(file, 0, SEEK_END);
       fileSize = ftell(file);
-      //rewind(file);
+      rewind(file);
       free(str);
     }
     exit(1);
@@ -145,7 +176,6 @@ int main(int argc, char *argv[]){
       month = getMonth();
       day = getDay();      
       getReminder(month, day);
-      //rewind(file);
       
       printf("\nWould you like to set a reminder (y/n)?\n");
       scanf("%c", &ans);
@@ -161,7 +191,7 @@ int main(int argc, char *argv[]){
 	input = fgets(str,150,stdin);
         strcat(str, time);
 	strcpy(&list1.remind[day], str);
-	snprintf(buffer, 150, "%s\n", &list1.remind[day]);
+        snprintf(buffer, BUFF, "%s %d: %s\n", ab,day, &list1.remind[day]);
 	fwrite(buffer, sizeof(char),sizeof(buffer), file);
 
 	printf("\nReminder set: \n");
@@ -171,5 +201,69 @@ int main(int argc, char *argv[]){
   
   fclose(file);
  
+ 
   return 0;
+}
+
+char *abbMonth(int m){
+
+  char *str;
+  
+  switch (m) {
+    
+  case 1:
+    str = "Jan";
+    return str;
+    break;
+  case 2:
+    str = "Feb";
+    return str;
+    break;
+  case 3:
+    str = "Mar";
+    return str;
+    break;
+  case 4:
+    str = "Apr";
+    return str;
+    break;
+  case 5:
+    str = "May";
+    return str;
+    break;
+  case 6:
+    str = "Jun";
+    return str;
+    break;
+  case 7:
+    str = "Jul";
+    return str;
+    break;
+  case 8:
+    str = "Aug";
+    return str;
+    break;
+  case 9:
+    str = "Sep";
+    return str;
+    break;
+  case 10:
+    str = "Oct";
+    return str;
+    break;
+  case 11:
+    str = "Nov";
+    return str;
+    break;
+  case 12:
+    str = "Dec";
+    return str;
+    break;
+  default :
+    str = "Date not found";
+    return str;
+    break;
+  
+  }
+  return str;
 }
