680acdf1a5f2c52a127fb76d30a04e9d20fc6697
MersenneInteger <zachjohnsonsoftware@gmail.com>
2017-09-28 17:31:05 -0500

    add time struct

--- reminder.h
+++ reminder.h
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
 
 #define BUFFER 150
 #define STORAGE_SIZE 1000
@@ -6,7 +11,12 @@
 char *curr_day;
 char *curr_month;
 char *curr_year;
+char month_abb[100];
 int reminder_count;
+char buffer[BUFFER];
+
+time_t today;
+struct tm * time_info;
 
 //struct to store date and reminder message
 struct reminder_list {
@@ -15,18 +25,17 @@ struct reminder_list {
 } Reminder;
 
 //arrray of structs to store reminders to write to a file
-struct reminder_list rem_storage[MAX_SIZE];
+struct reminder_list rem_storage[STORAGE_SIZE];
 
 //gets current month, day, and year
-char *get_month();
-char *get_day();
-char *get_year();
+int get_month();
+int get_day();
+char *get_month_abb();
 char *get_curr_time(); 
-int get_reminder_count(File *file);
+int get_reminder_count(FILE *file);
 
 //sets reminder date and message
-void set_date();
 void set_message();
 void read_reminder(FILE *file, struct reminder_list r);
-void write_reminder(File *file, struct reminder_list r, char *date, char *message);
-void delete_prev_reminders(File *file, struct reminder_list r);
+void write_reminder(FILE *file, struct reminder_list r, char *date, char *message);
+void delete_prev_reminders(FILE *file, struct reminder_list r);
