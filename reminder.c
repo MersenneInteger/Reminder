2d09ab3aa6d789309fa81cb11231d54a82a47f87
MersenneInteger <zachjohnsonsoftware@gmail.com>
2017-09-28 16:12:59 -0500

    set functions

--- reminder.c
+++ reminder.c
@@ -1,71 +1,66 @@
 #include <stdio.h>
-#include <string.h>
-#include <time.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <ctype.h>
-#include <assert.h>
+#include "reminder.h"
 
 /***************************/
 int get_month()
 {
-
-        time_t mytime; 
-        struct tm * timeInfo; //creat struct to store time
-        time(&mytime); //call time func
-        timeInfo = localtime(&mytime); //store local time in struct
-
-        return (timeInfo->tm_mon + 1); //returns the month 
+    time(&today); 
+    time_info = localtime(&today);
+    return (time_info->tm_mon + 1);
 }
 
 int get_day()
 {
-
-        time_t mytime; //creat struct to store time
-        struct tm * timeInfo; //call time func
-        time(&mytime); 
-        timeInfo = localtime(&mytime); //store local time in struct
-
-        return (timeInfo->tm_mday); //return day
+    time(&today); 
+    timeInfo = localtime(&today); 
+    return (timeInfo->tm_mday); 
 }
 
 char *get_curr_time()
 {
+    time(&loctime);
+    today = malloc(sizeof((ctime(&loctime))));
+    today = ctime(&loctime);
+    return today;
+}
 
-        time_t mytime;
-        time(&mytime);
-        char *string = (ctime(&mytime)); //store time as a string, includes day and month
-        return string;
+void set_date()
+{
 
 }
 
 int set_day()
 {
-        int day;
-        printf("For which day? ");
-        scanf("%d", &day);
-        if(day <= 0 || day > 31){  
-                printf("Incorrect input, enter a date between 1-31: ");
-                scanf("%d", &day);
-        }
-        return day;
+    int day;
+    printf("For which day? ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &day);
+      
+    while(day < 1 || day > 31){   
+        printf("Incorrect input, enter a date between 1-31: ");
+        fgets(line, sizeof(line), stdin);
+        sscanf(line, "%d", &day);
+    }
+    return day;
 }
 
 int set_month()
 {
-        int month;
-        printf("Which month would you like to create a reminder for?\n");
-        scanf("%d", &month);
-
-        if(month <= 0 || month > 12) {
-                printf("Incorrect input, enter a month between 1-12: ");
-                scanf("%d", &month);
-        }
-        return month;
+    int month;
+    printf("Which month would you like to create a reminder for?\n");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &month);
+    
+    while(month < 1 || month > 12) {
+        printf("Incorrect input, enter a month between 1-12: ");
+        fgets(line, sizeof(line), stdin);
+        sscanf(line, "%d", &month);
+    }
+    return month;
 }
-
 
 int main(int argc, char *argv[])
 {
-
+    char *curr_time = get_curr_time();
+    set_day();
 }
