e6fd20124ad5a47a0d352f87d556ff08aafdce5e
MersenneInteger <zachjohnsonsoftware@gmail.com>
2017-10-02 18:05:35 -0500

    delete old reminders

--- reminder.c
+++ reminder.c
@@ -58,7 +58,7 @@ void set_date()
 
     struct tm *future_time;
     future_time = localtime(&today);
-    future_time->tm_mon = m -1;
+    future_time->tm_mon = m-1;
     future_time->tm_mday = d;
 
     mktime(future_time);
@@ -71,8 +71,21 @@ void set_message()
     snprintf(Reminder.message, sizeof(Reminder.message), "%s", buffer);
 }
 
+void file_err_check(FILE *f)
+{
+    if(f == NULL) {
+        fprintf(stderr, "errno :%d\n", errno);
+        perror("Error");
+        exit(EXIT_FAILURE);
+    } 
+}
+
 void read_reminder()
 {
+    fclose(file);
+    file = fopen(".reminders.bin", "rb");
+    file_err_check(file);
+
     while(fread(&Reminder, sizeof(Reminder), 1, file))
         if(!strcmp(tod, Reminder.date))
             printf("%s\n%s\n", Reminder.date, Reminder.message);
@@ -87,49 +100,61 @@ void write_reminder()
 
         fclose(file);
         file = fopen(".reminders.bin", "ab");
-        if(file == NULL) {
-            fprintf(stderr, "errno :%d\n", errno);
-            perror("Error");
-            exit(EXIT_FAILURE);
-        }
+        file_err_check(file);
+
         set_date();
         set_message();
         fwrite(&Reminder, sizeof(Reminder), 1, file);
-    } else exit(EXIT_SUCCESS);
+    } else return;
 }
 
-void delete_prev_reminders()
+void delete_prev_reminders(FILE *f)
 {
+    fclose(f);
+    f = fopen(".reminders.bin", "rb+");
+    FILE *new_file = fopen(".temp.bin", "ab");
+    file_err_check(file);
+    file_err_check(new_file);
 
+    int d = get_day();
+    int m = get_month();
+    char tmp[DATE_LIMIT];
+    snprintf(tmp, sizeof(tmp), "%s", tod);
+    memmove(tmp, tmp + 4, 10);
+
+    while(fread(&Reminder, sizeof(Reminder), 1, file))
+        if(strcmp(tmp, Reminder.date) > 0)
+            fwrite(&Reminder.date, sizeof(Reminder), 1, new_file);
+    
+    int rem_err = remove(".reminders.bin");
+    if(rem_err) {
+        perror("file removal failure");
+        return;
+    }
+    int ren_err = rename(".temp.bin", ".reminders.bin");
+    if(ren_err) {
+        perror("rename failed");
+        return;
+    }
+    fclose(new_file);
 }
 
-void sort_reminders()
-{
-
-}
 void startup()
 {
     get_curr_time();
-
-    file = fopen(".reminders.bin", "rb");
-    if(file == NULL) {
-        fprintf(stderr, "errno :%d\n", errno);
-        perror("Error");
-        exit(EXIT_FAILURE);
-    } 
+    file = fopen(".reminders.bin", "ab");
+    file_err_check(file);
 }
 
 /***************************** M A I N *******************************************/
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     startup();
     printf("%s\n", tod);
-
     read_reminder(); 
     write_reminder();
-    printf("-------------\n");
-    read_reminder();
+    delete_prev_reminders(file);
 
     fclose(file);
     free(tod);
--- reminder.h
+++ reminder.h
@@ -5,23 +5,22 @@
 #include <time.h>
 #include <ctype.h>
 
-#define BUFFER 150
-#define STORAGE_SIZE 1000
+#define BUFF_SIZE 150
 #define DATE_LIMIT 50
 
 FILE *file;
 
 char *tod;
 char month_abb[100];
-char buffer[BUFFER];
+char buffer[BUFF_SIZE];
 
 time_t today;
 struct tm * time_info;
 
 //struct to store date and reminder message
 struct reminder_list {
-	char date[BUFFER];
-	char message[BUFFER];
+	char date[BUFF_SIZE];
+	char message[BUFF_SIZE];
 } Reminder;
 
 //gets current month, day, and year
@@ -34,6 +33,5 @@ char *get_curr_time();
 void set_message();
 void read_reminder();
 void write_reminder();
-void delete_prev_reminders();
-void sort_reminders();
+void delete_prev_reminders(FILE *file);
 void startup();
